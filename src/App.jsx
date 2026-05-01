b55622c6aefda46ec4a51a88c3d705baac4ff9f2
MichaelPrendiville <Michael@prendiville.com.au>
2026-05-01 20:05:44 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -319,9 +319,9 @@ function PublicSite({ faqs, suppliers, resources, onGoAdmin, suppliersBanner, re
                   </button>
                 ))}
               </nav>
-              <a href="https://fixion.com.au" target="_blank" rel="noreferrer" style={{ display: "flex", alignItems: "center", padding: "12px 0", borderBottom: `1px solid ${T.border}`, textDecoration: "none", gap: 10 }}>
-                <img src={FIXION_SRC} alt="Fixion" style={{ height: 40, width: "auto" }} />
-                <span style={{ fontFamily: T.fontSans, fontSize: 14, color: T.textMuted }}>Find it. Fix it.</span>
+              <a href="https://fixion.com.au" target="_blank" rel="noreferrer" style={{ display: "flex", alignItems: "flex-end", padding: "12px 0", borderBottom: `1px solid ${T.border}`, textDecoration: "none", gap: 10 }}>
+                <img src={FIXION_SRC} alt="Fixion" style={{ height: 40, width: "auto", display: "block" }} />
+                <span style={{ fontFamily: T.fontSans, fontSize: 13, color: T.textMuted, lineHeight: 1, paddingBottom: 2 }}>Find it. Fix it.</span>
               </a>
               <div style={{ borderTop: `1px solid ${T.border}`, paddingTop: 28, marginTop: 28 }}>
                 <p style={{ fontFamily: T.fontMono, fontSize: 10, color: T.textXMuted, letterSpacing: "0.15em", textTransform: "uppercase", marginBottom: 14 }}>For more information</p>
@@ -377,9 +377,9 @@ function PublicSite({ faqs, suppliers, resources, onGoAdmin, suppliersBanner, re
                     </button>
                   ))}
                 </nav>
-                <a href="https://fixion.com.au" target="_blank" rel="noreferrer" style={{ display: "flex", alignItems: "center", padding: "12px 0", borderBottom: `1px solid ${T.border}`, textDecoration: "none", gap: 10 }}>
-                  <img src={FIXION_SRC} alt="Fixion" style={{ height: 40, width: "auto" }} />
-                  <span style={{ fontFamily: T.fontSans, fontSize: 14, color: T.textMuted }}>Find it. Fix it.</span>
+                <a href="https://fixion.com.au" target="_blank" rel="noreferrer" style={{ display: "flex", alignItems: "flex-end", padding: "12px 0", borderBottom: `1px solid ${T.border}`, textDecoration: "none", gap: 10 }}>
+                  <img src={FIXION_SRC} alt="Fixion" style={{ height: 40, width: "auto", display: "block" }} />
+                  <span style={{ fontFamily: T.fontSans, fontSize: 13, color: T.textMuted, lineHeight: 1, paddingBottom: 2 }}>Find it. Fix it.</span>
                 </a>
               <div style={{ borderTop: `1px solid ${T.border}`, paddingTop: 28, marginTop: 28 }}>
                   <p style={{ fontFamily: T.fontMono, fontSize: 10, color: T.textXMuted, letterSpacing: "0.15em", textTransform: "uppercase", marginBottom: 14 }}>For more information</p>
